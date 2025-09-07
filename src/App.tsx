204805967031f151f429cde782b1e892b71fb260
iulii sobol <soboliulii@gmail.com>
2025-09-07 18:37:53 +0300

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,62 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-
-import { Routes, Route, Navigate } from 'react-router-dom';
-
-import { Navigation } from './components/Navigation';
-import { HomePage } from './components/HomePage';
+import {
+  Navigate,
+  NavLink,
+  Route,
+  Routes,
+  useLocation,
+} from 'react-router-dom';
+import cn from 'classnames';
 import { TabsPage } from './components/TabsPage';
-import { NotFoundPage } from './components/NotFoundPage';
 import React from 'react';
 
-export const App = () => (
-  <>
-    <Navigation />
+export const App = () => {
+  const { pathname } = useLocation();
 
-    <div className="section">
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/home" element={<Navigate to="/" replace />} />
+  return (
+    <>
+      <nav
+        className="navbar is-light is-fixed-top is-mobile has-shadow"
+        data-cy="Nav"
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <NavLink
+              to="/"
+              className={cn('navbar-item', { 'is-active': pathname === '/' })}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/tabs"
+              className={cn('navbar-item', {
+                'is-active': pathname.startsWith('/tabs'),
+              })}
+            >
+              Tabs
+            </NavLink>
+          </div>
+        </div>
+      </nav>
 
-          <Route path="tabs">
-            <Route index element={<TabsPage />} />
-            <Route path=":tabId" element={<TabsPage />} />
-          </Route>
-
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+      <div className="section">
+        <div className="container">
+          <Routes>
+            <Route path="/home" element={<Navigate to="/" replace />} />
+            <Route path="/" element={<h1 className="title">Home page</h1>} />
+            <Route path="tabs">
+              <Route index element={<TabsPage />}></Route>
+              <Route path=":tabId" element={<TabsPage />}></Route>
+            </Route>
+            <Route
+              path="*"
+              element={<h1 className="title">Page not found</h1>}
+            />
+          </Routes>
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
--- src/components/TabsPage.tsx
+++ src/components/TabsPage.tsx
@@ -1,40 +1,37 @@
-import { Link, useParams } from 'react-router-dom';
+import cn from 'classnames';
 import React from 'react';
-import { Tab } from '../types/Tab';
-
-const tabs: Tab[] = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-];
+import { NavLink, useParams } from 'react-router-dom';
 
 export const TabsPage = () => {
-  const { tabId } = useParams();
-  const currentTab = tabs.find(tab => tab.id === tabId);
+  const tabs = [
+    { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+    { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+    { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  ];
+  const { tabId } = useParams<{ tabId: string }>();
 
   return (
     <>
       <h1 className="title">Tabs page</h1>
-
       <div className="tabs is-boxed">
         <ul>
-          {tabs.map(tab => (
+          {tabs.map((tab, index) => (
             <li
               key={tab.id}
               data-cy="Tab"
-              className={tab.id === tabId ? 'is-active' : ''}
+              className={cn({ 'is-active': tabId === tab.id })}
             >
-              <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
+              <NavLink to={`/tabs/tab-${index + 1}`} className="navbar-item">
+                {tab.title}
+              </NavLink>
             </li>
           ))}
         </ul>
       </div>
 
       <div className="block" data-cy="TabContent">
-        {tabId
-          ? currentTab
-            ? currentTab.content
-            : 'Please select a tab'
+        {tabId && +tabId.split('').slice(-1) <= tabs.length
+          ? `Some text ${tabId.split('').slice(-1)}`
           : 'Please select a tab'}
       </div>
     </>
