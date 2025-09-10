0a5a19f1c7baa50bd803c98e46f206f9a57e06e3
iulii sobol <soboliulii@gmail.com>
2025-09-10 13:46:56 +0300

    fixed_01

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,35 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-import {
-  Navigate,
-  NavLink,
-  Route,
-  Routes,
-  useLocation,
-} from 'react-router-dom';
-import cn from 'classnames';
-import { TabsPage } from './components/TabsPage';
+import { Outlet, NavLink } from 'react-router-dom';
+import classNames from 'classnames';
 import React from 'react';
 
-export const App = () => {
-  const { pathname } = useLocation();
+const LinkClass = ({ isActive }: { isActive: boolean }) =>
+  classNames('navbar-item', { 'is-active': isActive });
 
-  return (
-    <>
-      <nav
-        className="navbar is-light is-fixed-top is-mobile has-shadow"
-        data-cy="Nav"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <NavLink
-              to="/"
-              className={cn('navbar-item', { 'is-active': pathname === '/' })}
-            >
-              Home
-            </NavLink>
-            <NavLink
-              to="/tabs"
-              className={cn('navbar-item', {
-                'is-active': pathname.startsWith('/tabs'),
-              })}
-            >
-              Tabs
-            </NavLink>
-          </div>
-        </div>
-      </nav>
-
-      <div className="section">
-        <div className="container">
-          <Routes>
-            <Route path="/home" element={<Navigate to="/" replace />} />
-            <Route path="/" element={<h1 className="title">Home page</h1>} />
-            <Route path="tabs">
-              <Route index element={<TabsPage />}></Route>
-              <Route path=":tabId" element={<TabsPage />}></Route>
-            </Route>
-            <Route
-              path="*"
-              element={<h1 className="title">Page not found</h1>}
-            />
-          </Routes>
+export const App = () => (
+  <>
+    <nav
+      className="navbar is-light is-fixed-top is-mobile has-shadow"
+      data-cy="Nav"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <NavLink to="/" className={LinkClass} end>
+            Home
+          </NavLink>
+          <NavLink to="tabs" className={LinkClass}>
+            Tabs
+          </NavLink>
         </div>
       </div>
-    </>
-  );
-};
+    </nav>
+
+    <div className="section">
+      <div className="container">
+        <Outlet />
+      </div>
+    </div>
+  </>
+);
